f8c4663399ada20b933fd38fd98ef703987d175b
BrionesDev <elianbrionesvictores65@gmail.com>
2025-07-15 14:58:30 +0200

    feat: delete task and fixed bugs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 import readline from "readline";
-import fs from "fs";
 
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
@@ -24,21 +23,15 @@ const mainMenu = () => {
 };
 
 const addTask = (description) => {
+    let tasks = readTasks();
+
     const newTask = {
         id: Math.random().toString(36).substring(2, 5),
-        description,
+        description: removeQuotes(description),
         status: "todo",
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString()
     };
-
-    let tasks = [];
-    try {
-        const fileContent = fs.readFileSync("./tasks.json", "utf8");
-        tasks = JSON.parse(fileContent);
-    } catch (error) {
-        console.error(error.message);
-    }
 
     tasks.push(newTask);
     fs.writeFileSync("./tasks.json", JSON.stringify(tasks, null, 2), "utf8");
@@ -48,10 +41,34 @@ const addTask = (description) => {
     mainMenu();
 };
 
-const updateTask = (taskId, description) => {
+const updateTask = (taskId, ...args) => {
+    let tasks = readTasks();
+
+    const task = findTaskById(tasks, taskId);
+    const description = args.join(" ");
+
+    task.description = removeQuotes(description);
+    task.updatedAt = new Date().toISOString();
+
+    fs.writeFileSync("./tasks.json", JSON.stringify(tasks, null, 2), "utf8");
+
+    console.log("- - - - - TASK UPDATED - - - - -");
+    console.table([task]);
+    mainMenu();
 };
 
 const deleteTask = (taskId) => {
+    let tasks = readTasks();
+
+    const task = findTaskById(tasks, taskId);
+
+    tasks.splice(taskId, 1);
+
+    fs.writeFileSync("./tasks.json", JSON.stringify(tasks, null, 2), "utf8");
+
+    console.log("- - - - - TASK DELETED - - - - -");
+    console.table([task]);
+    mainMenu();
 };
 
 const markAsTodo = (taskId) => {};
@@ -62,7 +79,7 @@ const markAsDone = (taskId) => {};
 
 const listAllTasks = () => {
     try {
-        const tasks = JSON.parse(fs.readFileSync("./tasks.json", "utf8"));
+        const tasks = readTasks();
 
         if (tasks.length === 0) {
             console.warn("\n= = = = = No tasks found! = = = = =\n");
@@ -94,19 +111,43 @@ const exit = () => {
 const handleUserInput = (input) => {
     const [command, ...args] = input.trim().split(" ");
 
-    if (args.length === 0 && (command === "list" || command === "exit")) return options[command]();
+    if (args.length === 0 && (command === "list" || command === "exit")) return actions[command]();
 
-    if (args.length === 1 && command === "list") return options[command + " " + args]();
+    if (args.length === 1 && command === "list") return actions[command + " " + args]();
 
-    if (command === "add" || command === "delete" || command.startsWith("mark")) return options[command](args[0]);
+    if (command === "add" || command === "delete" || command.startsWith("mark")) return actions[command](args[0]);
 
-    if (command === "update") return options[command](args[0], args[1]);
+    if (command === "update") {
+        const [taskId, ...description] = args;
+        return actions[command](taskId, ...description);
+    }
 
     console.error(`Invalid command: (${command}). Please try again.`);
     return mainMenu();
 };
 
-const options = {
+const readTasks = () => {
+    try {
+        return JSON.parse(fs.readFileSync("./tasks.json", "utf8"));
+    } catch (error) {
+        console.error(error.message);
+        return [];
+    }
+};
+
+const findTaskById = (tasks, taskId) => {
+    const task = tasks.find(task => task.id === taskId);
+
+    if (!task) {
+        console.error(`Task with ID ${taskId} not found.`);
+        return mainMenu();
+    }
+    return task;
+};
+
+const removeQuotes = text => text.replace(/["']/g, "");
+
+const actions = {
     "add": addTask,
     "update": updateTask,
     "delete": deleteTask,
